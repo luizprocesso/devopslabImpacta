de20d320bbfbf817dc0cdc1ff0b8ef783895206f
Luiz Processo <34315141+luizprocesso@users.noreply.github.com>
2023-03-11 10:18:21 -0300

    bug lines removed

--- app.py
+++ app.py
@@ -7,16 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hey ho lets go!!!"
-
-# @app.route('/bug')                                                                                                                                
-# def bad():                                                                                                                                        
-#     try:                                                                                                                                          
-#         raise TypeError()                                                                                                                         
-#     except TypeError as e:                                                                                                                        
-#         print(e)                                                                                                                                  
-#     except TypeError as e:                                                                                                                        
-#         print("Duplicado, ou seja, nunca vai entrar aqui.")    
+    return "Hey ho lets go!!!"  
 
 if __name__ == '__main__':
     app.run()
